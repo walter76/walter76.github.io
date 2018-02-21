22d75ca9f5de6c501f9444add083d648d8c516ab
walter <walter.stocker@gmx.de>
2018-02-21 22:37:57 +0100

    first version of blog

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "How to Work on a Branch with GitHub",
+        "excerpt":"Create the remote branch on github Create a branch in the local repo Set the remote branch as upstream branch with git branch --set-upstream-to=origin/source source. You can now use git pull/push to synchronize your work. You can also pull/push the remote branch with git pull origin source Switch branches with...","categories": ["git"],
+        "tags": [],
+        "url": "https://walter76.github.io/git/how-to-work-on-a-branch-with-github/",
+        "teaser":null}]
