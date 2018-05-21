c8f9a4e3b383a07bd9273bb3fdcea595cff55e8f
walter <walter.stocker@gmx.de>
2018-05-21 23:05:01 +0200

    docs: updated the post on working with remote branches

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,11 @@
 var store = [{
         "title": "How to Work on a Branch with GitHub",
-        "excerpt":"Create the remote branch on github Create a branch in the local repo Set the remote branch as upstream branch with git branch --set-upstream-to=origin/source source. You can now use git pull/push to synchronize your work. You can also pull/push the remote branch with git pull origin source Switch branches with...","categories": ["git"],
+        "excerpt":"In this post I will show how to create a branch if your repository is on GitHuband what you have to do to synchronize it with your local repository. Create the Remote Branch on GitHub First you need to create a remote branch on GitHub. This branch is afterwardsused to...","categories": ["git"],
         "tags": [],
         "url": "https://walter76.github.io/git/how-to-work-on-a-branch-with-github/",
+        "teaser":null},{
+        "title": "How to Set-up a Vagrant box for Jekyll",
+        "excerpt":"Prerequisites First I had to install the most up-to-date versions of Virtual Box andVagrant on my Laptop. Virtual Box is used by Vagrant for the Virtual Machines. In additionalso a version of PowerShell &gt; 2 is needed. If you are using Windows 10, you should already have it installed. As...","categories": ["vagrant","jekyll"],
+        "tags": [],
+        "url": "https://walter76.github.io/vagrant/jekyll/vagrant-box-for-jekyll/",
         "teaser":null}]
